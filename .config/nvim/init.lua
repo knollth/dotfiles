939499a99348b793d179d6e36e042e1178625da4
knollth <tom@knollth.dev>
2025-12-25 16:04:31 +0100

    add options back to init.lua, change tabwidth to 2 spaces, change diagonstics to <leader>d and oil to <leader>e

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,24 @@
-require("options")
 require("keymaps")
 require("lsp")
 
+vim.opt.swapfile = false
+vim.opt.winborder = "rounded"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes" -- extra column for warnings before linenum
+vim.opt.expandtab = true  --  spaces instead of shifts
+vim.opt.shiftwidth = 2    -- 3 spaces indent
+vim.opt.tabstop = 2
+vim.g.mapleader = " "
+
+
 vim.pack.add({
-	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/echasnovski/mini.pairs" },
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+    { src = "https://github.com/vague2k/vague.nvim" },
+    { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/echasnovski/mini.pairs" },
+    { src = "https://github.com/echasnovski/mini.pick" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/chomosuke/typst-preview.nvim" },
 })
 
 require "mini.pick".setup()
@@ -16,4 +26,3 @@ require "mini.pairs".setup()
 require "oil".setup()
 
 vim.cmd("colorscheme vague")
-
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,10 +1,12 @@
 -- vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 -- vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
--- vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
+
 
 
