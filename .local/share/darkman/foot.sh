dab998093922874e5642424f7e740c396540fcf6
knollth <tom@knollth.dev>
2026-06-08 09:30:47 +0200

    adjust foot config to newer version

--- .local/share/darkman/foot.sh
+++ .local/share/darkman/foot.sh
@@ -2,11 +2,11 @@
 
 case "$1" in
 dark)
-	echo "initial-color-theme=1" >~/.config/foot/colors.ini
+	echo "initial-color-theme=dark" >~/.config/foot/colors.ini
 	killall -s SIGUSR1 foot
 	;;
 light)
-	echo "initial-color-theme=2" >~/.config/foot/colors.ini
+	echo "initial-color-theme=light" >~/.config/foot/colors.ini
 	killall -s SIGUSR2 foot
 	;;
 default) exit 1 ;;
